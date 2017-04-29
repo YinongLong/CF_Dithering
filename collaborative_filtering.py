89a74b0ce101afeb7e0d011dc358ace7ed7667e0
YinongLong <pleasecallmegeek@gmail.com>
2017-04-29 16:15:06 +0800

    added two new types of predicting method, which are about dithering methods.

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -6,6 +6,7 @@ Created on 2017/4/18 15:01
 @file: collaborative_filtering.py
 
 关于用户和物品的相似度计算采用0/1评分方式
+
 """
 from __future__ import print_function
 from __future__ import division
@@ -56,6 +57,61 @@ class UserCF(object):
         denominator = np.sqrt(norm_user_1) * np.sqrt(norm_user_2)
         return numerator / denominator
 
+    def dithering_predict(self, user_id, alpha=None, beta=None, k=10, num_dithering=10, num_users=5, seed=1, **kargs):
+        """
+        根据指定的用户ID，计算与其最为相似的num_dithering个用户，首先对这些相似的用户根据参数alpha和beta进行抖动，
+        然后使用num_users用户的历史记录进行推荐计算。
+        :param user_id: int，指定的用户ID
+        :param alpha: float，抖动的参数，如果为None，代表不进行抖动
+        :param beta: float，抖动的参数
+        :param k: int，给用户推荐的物品数
+        :param num_dithering: int，参与抖动的用户个数
+        :param num_users: int，用来进行推荐计算的用户个数
+        :param seed: int，设置随机数生成器的种子，使得实验可以复现
+        :return: 返回给用户推荐的k个物品ID，以及物品ID在未抖动结果中的rank
+        """
+        candidates = list()
+        for uid in self.user_data.keys():
+            if uid != user_id:
+                candidates.append((uid, self._similarity_user(user_id, uid)))
+
+        # 选择出最为相似的num_dithering个用户
+        candidates = sorted(candidates, key=lambda item: item[1], reverse=True)[:num_dithering]
+        used_items = self.user_data[user_id]
+        # 计算抖动范围内的原始推荐结果
+        original = collections.defaultdict(np.float32)
+        for uid, similarity in candidates:
+            records = self.user_data[uid]
+            for item_id, score in records.items():
+                if item_id not in used_items:
+                    original[item_id] += score * similarity
+        original = sorted(original.items(), key=lambda item: item[1], reverse=True)
+        original_order = {item[0]: index for index, item in enumerate(original, start=1)}
+        # 对推荐候选用户进行抖动
+        if (alpha is not None) and (beta is not None):
+            scores = list()
+            np.random.seed(seed)
+            for i in range(num_dithering):
+                uid, _ = candidates[i]
+                score = alpha * np.log(i + 1.0) + (1 - alpha) * np.random.normal(0., np.sqrt(np.sqrt(np.log(beta))))
+                scores.append((uid, score))
+            candidates = dict(candidates)
+            # 根据抖动的结果进行排序
+            scores.sort(key=lambda item: item[1], reverse=True)
+            temp_candidates = [(uid, candidates[uid]) for uid, _ in scores]
+            candidates = temp_candidates
+
+        result = collections.defaultdict(np.float32)
+        # 选择出用来计算推荐的用户
+        candidates = candidates[:num_users]
+        for uid, similarity in candidates:
+            records = self.user_data[uid]
+            for item_id, score in records.items():
+                if item_id not in used_items:
+                    result[item_id] += similarity * score
+        result = sorted(result.items(), key=lambda item: item[1], reverse=True)
+        return [result[i][0] for i in range(k)], [original_order[result[i][0]] for i in range(k)]
+
     def predict_single(self, user_id, k=10, num_users=5):
         """
         对指定的单个用户ID，计算最有可能的k个推荐
@@ -65,17 +121,17 @@ class UserCF(object):
         :return: list，推荐的物品ID列表
         """
         candidates = list()
-        for cid in self.user_data.keys():
-            if cid != user_id:
-                candidates.append((cid, self._similarity_user(user_id, cid)))
+        for uid in self.user_data.keys():
+            if uid != user_id:
+                candidates.append((uid, self._similarity_user(user_id, uid)))
 
         # 选择出最相似的num_users个用户
-        candidates = sorted(candidates, key=lambda item: item[0], reverse=True)[:num_users]
+        candidates = sorted(candidates, key=lambda item: item[1], reverse=True)[:num_users]
 
         result = collections.defaultdict(np.float32)
         used_items = self.user_data[user_id]
-        for cid, similarity in candidates:
-            records = self.user_data[cid]
+        for uid, similarity in candidates:
+            records = self.user_data[uid]
             for item_id, score in records.items():
                 # 过滤掉用户已经使用过的物品
                 if item_id not in used_items:
@@ -130,6 +186,39 @@ class ItemCF(object):
         denominator = np.sqrt(norm_item_1) * np.sqrt(norm_item_2)
         return numerator / denominator
 
+    def dithering_predict(self, user_id, alpha=None, beta=None, k=10, num_dithering=10, seed=1, **kargs):
+        """
+        根据指定的用户ID，给用户推荐k个物品，其中推荐物品来自对num_dithering个物品抖动后的结果，因此
+        指定的参数中必须满足num_dithering >= k
+        :param user_id: int，指定的用户ID
+        :param alpha: float，抖动操作的参数，如果为None，则代表不进行抖动操作
+        :param beta: float，抖动操作的参数，如果为None，则代表不进行抖动操作
+        :param k: int，推荐的物品个数
+        :param num_dithering: int，抖动的物品个数
+        :param seed: int，随机数生成器的种子，使得实验可以复现
+        :return: 返回推荐的k个物品的ID，以及推荐结果在原始未抖动结果的rank
+        """
+        used_items = self.user_data[user_id]
+        candidates = collections.defaultdict(np.float32)
+        # 计算所有指定用户为评分的物品与用户已经评分的物品的相似度
+        for iid in self.item_data.keys():
+            if iid not in used_items:
+                for item_id, score in used_items.items():
+                    candidates[iid] += self._similarity_item(iid, item_id) * score
+        candidates = sorted(candidates.items(), key=lambda item: item[1], reverse=True)[:num_dithering]
+        original_order = {item[0]: index for index, item in enumerate(candidates, start=1)}
+        if (alpha is not None) and (beta is not None):
+            scores = list()
+            for i in range(num_dithering):
+                iid, _ = candidates[i]
+                score = alpha * np.log(i + 1.0) + (1 - alpha) * np.random.normal(0., np.sqrt(np.sqrt(np.log(beta))))
+                scores.append((iid, score))
+            scores.sort(key=lambda item: item[1], reverse=True)
+            candidates = dict(candidates)
+            temp_candidates = [(iid, candidates[iid]) for iid, _ in scores]
+            candidates = temp_candidates[:k]
+        return [candidates[i][0] for i in range(k)], [original_order[candidates[i][0]] for i in range(k)]
+
     def predict_single(self, user_id, k=10):
         """
         根据指定的用户ID，给用户推荐top-K个电影
@@ -170,100 +259,72 @@ def process_test_data(rating_data):
     return dict_data
 
 
-def dithering(recommendation, alpha, beta, seed=None):
-    """
-    对推荐列表recommendation进行抖动
-    :param recommendation: list，根据推荐的强度从大到小排序的电影推荐列表
-    :param alpha: float，抖动的alpha参数，其范围在0-1
-    :param beta: float，抖动的beta参数
-    :param seed: int，方便实验的重现
-    :return: list，返回抖动后的推荐列表
-    """
-    len_rec = len(recommendation)
-    result = list()
-    if seed is not None:
-        np.random.seed(seed)
-    for i in range(1, len_rec + 1):
-        item_id = recommendation[i-1]
-        score = alpha * np.log(i) + (1 - alpha) * np.random.normal(loc=0.0, scale=np.log(beta))
-        result.append((item_id, score))
-    result.sort(key=lambda item: item[1], reverse=True)
-    return [item[0] for item in result]
-
-
-def metric_precision(prediction, goals, movie_info):
+def metric_precision(prediction, goals):
     """
     计算推荐结果的准确率，即所有推荐结果中，包含在用户已经评分的物品的比例
     :param prediction: list，预测的推荐列表
     :param goals: list，用户实际上已经评分的物品
-    :param movie_info: dict，存储电影ID以及其对应的名称
-    :return: float，推荐结果中，命中用户兴趣的比例大小
+    :return: 返回推荐结果的precision，以及命中的推荐物品ID
     """
     count = 0
     hit = 0
     hit_movies = list()
-    print('The prediction results are:')
-    prediction_order = {}
     for item_id in prediction:
-        print(movie_info[item_id], end=' | ')
         count += 1
-        prediction_order[item_id] = count
         if item_id in goals:
             hit += 1
             hit_movies.append(item_id)
-    print()
-    return hit / count, hit_movies, prediction_order
+    return hit / count, hit_movies
 
 
-def print_prediction_result(result, hit_movies, movies_info):
-    print('==' * 40)
-    print('推荐结果的准确率 %.2f' % result)
-    print()
-    print('==' * 40)
-    print('推荐命中电影: ')
-    for item_id in hit_movies:
-        print(movies_info[item_id], end=' | ')
-    print(end='\n\n')
-    print('==' * 40)
-
-
-def test_dithering(prediction, movie_info, prediction_order, seed=1):
+def evaluate_dithering(user_id, method, movie_info, goals, k=10):
     """
-    对系统的推荐进行抖动，观察效果
-    :param prediction: list，推荐算法直接预测的结果
-    :param movie_info: dict，电影ID对应的电影名称
-    :param prediction_order: dict，记录原始推荐的物品的排序
-    :param seed: int，设置随机数的种子，方便实验的重现
+    对Dithering的效果进行评估
+    :param user_id: int，待推荐物品的用户ID
+    :param method: object，待评估的推荐方法，可调用其dithering_predict方法
+    :param movie_info: dict，电影ID对用的电影名称
+    :param goals: list，用户现实中评分的物品，即预测推荐的目标
+    :param k: int，给用户推荐的物品数
     :return: None
     """
     alphas = [0.1, 0.1, 0.1, 0.5, 0.5, 0.5, 0.9, 0.9, 0.9]
     betas = [1.1, 2.0, 5.0, 1.1, 2.0, 5.0, 1.1, 20.0, 5.0]
+
     result = dict()
-
+    # 推荐不进行抖动的结果
+    original, original_rank = method.dithering_predict(user_id, alpha=None, beta=None)
+    # 推荐利用抖动的结果
     for alpha, beta in zip(alphas, betas):
-        temp_result = dithering(prediction, alpha, beta, seed)
-        result[(alpha, beta)] = temp_result
+        recommendations, rank = method.dithering_predict(user_id, alpha=alpha, beta=beta, seed=1)
+        result[(alpha, beta)] = (recommendations, rank)
 
-    print('原始未抖动的推荐结果为：')
-    for item_id in prediction:
-        print(item_id, end=' ')
-    print(end='\n\n\n')
+    precision, hit_movies = metric_precision(original, goals)
+    print('对用户%d的原始推荐结果precision=%f，具体推荐为：' % (user_id, precision))
+    for iid in original:
+        print(movie_info[iid], end=' | ')
+    print('\n\n')
+    print('原始推荐结果的rank: ')
+    for index in original_rank:
+        print(index, end=' ')
+    print('\n\n')
 
-    for key, values in result.items():
-        print('--' * 40)
-        print('抖动参数 alpha=%f and beta=%f' % (key[0], key[1]), end='\n\n')
-        print('抖动后的物品ID:', end=' ')
-        for item_id in values:
-            print(item_id, end=' ')
-        print(end='\n\n')
-        print('抖动后对应的电影名称:', end=' ')
-        for item_id in values:
-            print(movie_info[item_id], end=' ')
-        print(end='\n\n')
-        print('抖动后对应原来推荐的排序:', end=' ')
-        for item_id in values:
-            print(prediction_order[item_id], end=' ')
-        print(end='\n\n\n')
+    print('推荐命中结果为：')
+    for iid in hit_movies:
+        print(movie_info[iid], end=' | ')
+    print('\n\n')
+
+    print('==' * 40)
+    print('具体的各种抖动后的结果为：')
+    for key, value in result.items():
+        alpha, beta = key
+        prediction, rank = value
+        precision, hit_movies = metric_precision(prediction, goals)
+        print('alpha=%f, beta=%f, precision=%f' % (alpha, beta, precision))
+        print('推荐结果的rank: ')
+        for index in rank:
+            print(index, end=' ')
+        print('\n\n')
+        print('==' * 40)
 
 
 def evaluation():
@@ -285,22 +346,16 @@ def evaluation():
     ucf = UserCF()
     ucf.train(rating_data)
     # 指定预测推荐的用户ID，以及推荐列表的长度
-    np.random.seed(10)
+    np.random.seed(1)
     user_id = np.random.randint(1, 944)
     print('待推荐的用户ID:', user_id)
     k = 10
-    # 给指定的用户进行推荐
-    prediction = ucf.predict_single(user_id, k)
 
     # 获取测试数据中，指定用户已经评分的物品
     goals = sorted(test_data[user_id].items(), key=lambda item: item[1], reverse=True)
     goals = [item_score[0] for item_score in goals]
 
-    precision, hit_movies, prediction_order = metric_precision(prediction, goals, movie_info)
-
-    print_prediction_result(precision, hit_movies, movie_info)
-
-    test_dithering(prediction, movie_info, prediction_order, seed=1)
+    evaluate_dithering(user_id, ucf, movie_info, goals, k=10)
 
 
 def main():
